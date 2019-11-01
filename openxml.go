dfbe01cc7e367eb0f016bf79506ca4d328c97383
DrSh4dow <daniel.morettiv@gmail.com>
2019-11-01 12:16:42 -0300

    se creo las estructuras bases del documento xml

---
+++ openxml.go
@@ -0,0 +1,16 @@
+package metadata
+
+import "encoding/xml"
+
+type OfficeCoreProperty struct {
+	XMLName        xml.Name `xml:"coreProperties"`
+	Creator        string   `xml:"creator"`
+	LastModifiedBy string   `xml:"lastModifiedBy"`
+}
+
+type OfficeAppProperty struct {
+	XMLName     xml.Name `xml:"Properties"`
+	Application string   `xml:"Application"`
+	Company     string   `xml:"Company"`
+	Version     string   `xml:"AppVersion"`
+}
