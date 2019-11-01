984fba3449f3d8cd974a1027362530da0169230b
DrSh4dow <daniel.morettiv@gmail.com>
2019-11-01 12:48:12 -0300

    se finalizo la creacion del mapeador de xml

--- openxml.go
+++ openxml.go
@@ -1,6 +1,10 @@
 package metadata
 
-import "encoding/xml"
+import (
+	"archive/zip"
+	"encoding/xml"
+	"strings"
+)
 
 type OfficeCoreProperty struct {
 	XMLName        xml.Name `xml:"coreProperties"`
@@ -13,4 +17,61 @@ type OfficeAppProperty struct {
 	Application string   `xml:"Application"`
 	Company     string   `xml:"Company"`
 	Version     string   `xml:"AppVersion"`
+}
+
+var OfficeVersions = map[string]string{
+	"16": "2016",
+	"15": "2013",
+	"14": "2010",
+	"12": "2007",
+	"11": "2003",
+}
+
+func (a *OfficeAppProperty) GetMajorVersion() string {
+	tokens := strings.Split(a.Version, ".")
+	if len(tokens) < 2 {
+		return "Unknown"
+	}
+	v, ok := OfficeVersions[tokens[0]]
+	if !ok {
+		return "Unknown"
+	}
+
+	return v
+}
+
+func NewProperties(r *zip.Reader) (*OfficeCoreProperty, *OfficeAppProperty, error) {
+	var coreProps OfficeCoreProperty
+	var appProps OfficeAppProperty
+
+	for _, f := range r.File {
+		switch f.Name {
+		case "docProps/core.xml":
+			if err := process(f, &coreProps); err != nil {
+				return nil, nil, err
+			}
+		case "docProps/app.xml":
+			if err := process(f, &appProps); err != nil {
+				return nil, nil, err
+			}
+		default:
+			continue
+		}
+	}
+
+	return &coreProps, &appProps, nil
+}
+
+func process(f *zip.File, prop interface{}) error {
+	rc, err := f.Open()
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	if err := xml.NewDecoder(rc).Decode(&prop); err != nil {
+		return err
+	}
+
+	return nil
 }
